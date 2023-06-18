a16de28a6ad0c589c2d30f4b247deba50e040487
KashanAdnan <khanzaidaboy@gmail.com>
2023-06-18 14:21:44 +0500

    some-code

--- Config/confg.js
+++ Config/confg.js
@@ -1,10 +1,10 @@
 const JWTSecret = "KaLM0IKjDfsWeAVbmIo2JsL3HmTlKleqq";
 const mongoDB =
   "mongodb+srv://kashan:kashan654321@cluster0.c6v8zv7.mongodb.net/?retryWrites=true&w=majority";
-  const PORT = 3000
-  
-  module.exports = {
-    JWTSecret,
-    mongoDB,
-    PORT,
-  };
+const PORT = process.env.PORT;
+
+module.exports = {
+  JWTSecret,
+  mongoDB,
+  PORT,
+};
